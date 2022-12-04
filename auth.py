fcf12155cbf4c574f4271e8e6ff4806d52c629ef
Aleksey Lagunov <aslagunov@avito.ru>
2022-12-04 17:04:15 +0300

    User with app_context

--- auth.py
+++ auth.py
@@ -4,7 +4,7 @@ from flask import Blueprint, render_template, redirect, url_for, request, flash
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, logout_user, login_required
 from models import User
-from main import db, app
+from main import db
 
 auth = Blueprint('auth', __name__)
 
@@ -20,8 +20,7 @@ def login_post():
     password = request.form.get('password')
     remember = True if request.form.get('remember') else False
 
-    with app.app_context():
-        user = User.query.filter_by(email=email).first()
+    user = User.query.filter_by(email=email).first()
 
     # check if user actually exists
     # take the user supplied password, hash it, and compare it to the hashed password in database
--- main.py
+++ main.py
@@ -29,8 +29,7 @@ def create_app():
     @login_manager.user_loader
     def load_user(user_id):
         # since the user_id is just the primary key of our user table, use it in the query for the user
-        with app.app_context():
-            user = User.query.get(int(user_id))
+        user = User.query.get(int(user_id))
         return user
 
     # blueprint for auth routes in our app
