b903971824f20afb4644e12eae980a9da02a4bb3
Aleksey Lagunov <aslagunov@avito.ru>
2022-12-04 16:49:11 +0300

    Fixed index

--- auth.py
+++ auth.py
@@ -30,7 +30,7 @@ def login_post():
 
     # if the above check passes, then we know the user has the right credentials
     login_user(user, remember=remember)
-    return redirect(url_for('main.data'))
+    return redirect(url_for('index.data'))
 
 
 @auth.route('/signup')
